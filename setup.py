80b108f3d1dea33452c3fe8fc6080b36f6ab49ba
Brendan Kohler <brendan@algorithmic.ly>
2017-03-22 10:01:28 -0400

    Add new release 0.2.0.0 with changes to AST.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os import path
 
 setup(
     name='sentenai',
-    version='0.1.2.0',
+    version='0.2.0.0',
     description='Client library for Sentenai',
     long_description="",
     url='https://github.com/sentenai/py-sentenai',
