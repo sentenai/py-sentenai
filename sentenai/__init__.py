b55bf422d13e2cba56d79e9520f42ce467553e23
D. Yu <dyu@fastmail.com>
2017-07-22 11:37:34 +0800

    small nitpicks in __init__.py

--- sentenai/__init__.py
+++ sentenai/__init__.py
@@ -1,9 +1,6 @@
 import json
 
-from sentenai.exceptions import *
-from sentenai.utils import *
-from sentenai.api import *
-from sentenai.flare import *
+from sentenai.flare import EventPath, InCircle, InPolygon, Par, Select, Span, Switch
 
 try:
     from urllib.parse import quote
@@ -21,18 +18,17 @@ LEFT, CENTER, RIGHT = range(-1, 2)
 DEFAULT = None
 
 
-#### Flare Objects
-
-
 #### Convenience Functions
-
 V = EventPath()
+
 
 def event(*args, **kwargs):
     return Switch(*args, **kwargs)
 
+
 def ast(q):
     return json.dumps(q(), indent=4)
+
 
 def select(start=None, end=None):
     """Select events from a span of time.
@@ -42,8 +38,10 @@ def select(start=None, end=None):
     end -- select events occuring before `datetime()`.
     """
     kwargs = {}
-    if start: kwargs['start'] = start
-    if end: kwargs['end'] = end
+    if start:
+        kwargs['start'] = start
+    if end:
+        kwargs['end'] = end
     return Select(**kwargs)
 
 
@@ -53,14 +51,18 @@ def span(*q, **kwargs):
     else:
         return Span(*q, **kwargs)
 
-def any_of(*q): return Par("any", q)
 
-def all_of(*q): return Par("all", q)
+def any_of(*q):
+    return Par("any", q)
+
+
+def all_of(*q):
+    return Par("all", q)
+
 
 def within_distance(km, of):
     return InCircle(of, km)
 
+
 def inside_region(poly):
     return InPolygon(poly)
-
-#### Non-Flare Objects
