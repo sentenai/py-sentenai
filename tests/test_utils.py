6a22b5f6d5c420b5545a696b7551ed4ead367b97
stites <sam@stites.io>
2017-07-27 14:56:00 -0400

    fix py2str error

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,17 @@
 from hypothesis import given, example, assume
 from hypothesis.strategies import text
 
-from sentenai.utils import (py2str)
+from sentenai.utils import py2str, PY3
 import string, unittest
 
 @given(text())
 @example(string.ascii_letters)
 def test_py2str(s):
-    assume(py2str(s) == s)
+    @py2str
+    class Foo:
+        def __str__(self):
+            return 'im a string'
+
+    assume(isinstance(str(s), str if PY3 else unicode))
 
 
