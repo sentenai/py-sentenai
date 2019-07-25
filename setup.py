2ead09c816a146aa4790e6c8dfb81da77690ce69
Brendan Kohler <brendan@sentenai.com>
2019-07-25 13:23:23 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sentenai',
-    version='1.0.0.0',
+    version='1.0.0.2',
     description='Client library for Sentenai',
     long_description="",
     url='https://github.com/sentenai/py-sentenai',
