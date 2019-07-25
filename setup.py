6684257a5ef20607f9e1d4c01c2385e7e7af8a13
Brendan Kohler <brendan@sentenai.com>
2019-07-25 00:23:57 -0400

    Add updates to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sentenai',
-    version='0.99.2.0',
+    version='1.0.0.0',
     description='Client library for Sentenai',
     long_description="",
     url='https://github.com/sentenai/py-sentenai',
