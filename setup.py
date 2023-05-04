b9a912e0354437c741273bcf8023874f5d388ca5
xnomagichash <brendan@sentenai.com>
2023-05-04 16:21:35 -0400

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sentenai',
-    version='1.6.4.6',
+    version='1.6.4.7',
     description='Client library for Sentenai',
     long_description="",
     url='https://github.com/sentenai/py-sentenai',
